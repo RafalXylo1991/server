4e788a6939085cf08206043475d21f811a03846c
xyloh <xylohunter1991@gmail.com>
2022-07-28 18:56:33 +0200

    sdffdhds

--- api.js
+++ api.js
@@ -8,9 +8,5 @@ app.get('/listUsers', function (req, res) {
   
 
 })
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.set('port', process.env.PORT || 3000);
+app.listen(3000);
