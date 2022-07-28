b3b92a0885e33b3af203ebe4a1be62daaa5cf4c1
xyloh <xylohunter1991@gmail.com>
2022-07-28 20:08:46 +0200

    sdffdhdduj;;lhddhkhlks

--- api.js
+++ api.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+
 const port = process.env.PORT || "1337";
 app.set("port", port);
-app.get('/', function (req, res) {
-   
-      res.end( "{cycuszki:cipeczka}" );
+var user = {
+    "user4" : {
+       "name" : "mohit",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+    }
+ }
+app.get('/f', function (req, res) {
+   fs.readFile("./user.json",'utf8',(err,data)=>{res.send(data)})
+      
   
 
 })
-
+app.post('/addUser', function (req, res) {
+    // First read existing users.
+   console.log("cipeczka")
+ })
 app.listen(port, () => console.log(`Server running on localhost:${port}`));
