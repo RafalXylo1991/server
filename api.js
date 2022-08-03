498e99416be40c9b1eb6a0921acec3bb7b769a7c
xyloh <xylohunter1991@gmail.com>
2022-08-03 20:37:23 +0200

    promilljlfghfgdsfjos;e

--- api.js
+++ api.js
@@ -37,7 +37,7 @@ router.get('/sendHours', async (req, res)=> {
 router.post('/addMonth', async(req,res)=>{
      sql.createTable(req.body.month)
 
-  res.json(req.body.month.month)
+  res.json(req.body)
 })
 router.get('/create', function(req,res){
  
