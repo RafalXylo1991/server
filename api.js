c88ec7557f25de5bd214d907093daa660a126b7f
xyloh <xylohunter1991@gmail.com>
2022-08-04 19:22:34 +0200

    sjjklje

--- api.js
+++ api.js
@@ -29,7 +29,7 @@ router.get("/", (req,res)=>{
 router.post('/sendHours', async (req, res)=> {
   const obj = Object.keys(req.body);
  
-console.log(obj[0])
+console.log(obj[0]['month'])
   
     
    
