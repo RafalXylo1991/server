d48fdfee23482600ecaf93e2843a998b381244de
xyloh <xylohunter1991@gmail.com>
2022-08-05 20:10:52 +0200

    s;jjehkhjlk

--- sql.js
+++ sql.js
@@ -34,7 +34,7 @@ const createTable=async(table)=>{
 }
 const insertData=async(data)=>{
   console.log(typeof(data.week))
-console.log("insert into  "+data.month+" values('"+data.data+"',"+parseInt(data.nadgodziny)+","+data.week=="true"?1:0+","+parseInt(data.saturday)+","+parseInt(data.sunday)+")")
+console.log("insert into  "+data.month+" values('"+data.data+"',"+parseInt(data.nadgodziny)+"," +data.week=="true"?1:0+","+data.saturday=="true"?1:0+","+data.sunday=="true"?1:0+")")
   client.query("insert into  "+data.month+" values('"+data.data+"',"+parseInt(data.nadgodziny)+"," +data.week=="true"?1:0+","+data.saturday=="true"?1:0+","+data.sunday=="true"?1:0+")", (err, res) => {
     if (err) throw err
     console.log(res)
